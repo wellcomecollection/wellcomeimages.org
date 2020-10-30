2c5e749b7df60ae162fef1512651b9e4dd7b74c9
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-30 09:13:12 +0000

    Remove some commented out code

--- terraform/edge_lambdas.tf
+++ terraform/edge_lambdas.tf
@@ -27,20 +27,11 @@ resource "aws_s3_bucket" "wellcomeimages" {
   tags = local.default_tags
 }
 
-# data "aws_s3_bucket_object" "edge_lambda_origin" {
-#   bucket = "wellcomeimages"
-#   key    = "lambdas/edge_lambda_origin.zip"
-# }
-
 resource "aws_lambda_function" "edge_lambda_request" {
   function_name = "wellcomeimages_edge_lambda_request"
   role          = aws_iam_role.edge_lambda_role.arn
   runtime       = "nodejs8.10"
   handler       = "edge_lambda_origin.handler"
-
-  # s3_bucket         = "${aws_s3_bucket_object.edge_lambda_request.bucket}"
-  # s3_key            = "edge_lambda_origin.zip"
-  # s3_object_version = "${data.aws_s3_bucket_object.edge_lambda_origin.version_id}"
 
   filename         = "../lambdas/edge_lambda_origin.zip"
   source_code_hash = filebase64sha256("../lambdas/edge_lambda_origin.zip")
