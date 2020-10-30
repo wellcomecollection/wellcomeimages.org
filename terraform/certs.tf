5578f859dc7ea574f972d40e9656e68f2be66582
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-30 09:26:00 +0000

    Manage the ACM certificate validation in Terraform

--- terraform/certs.tf
+++ terraform/certs.tf
@@ -10,3 +10,32 @@ resource "aws_acm_certificate" "wellcomeimages" {
     create_before_destroy = true
   }
 }
+
+data "aws_route53_zone" "wellcomeimages" {
+  provider = aws.dns
+
+  name = "wellcomeimages.org."
+}
+
+resource "aws_route53_record" "wellcomeimages" {
+  provider = aws.dns
+
+  for_each = {
+    for dvo in aws_acm_certificate.wellcomeimages.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  ttl     = 300
+  type    = each.value.type
+  zone_id = data.aws_route53_zone.wellcomeimages.zone_id
+}
+
+resource "aws_acm_certificate_validation" "wellcomeimages" {
+  certificate_arn         = aws_acm_certificate.wellcomeimages.arn
+  validation_record_fqdns = [for record in aws_route53_record.wellcomeimages : record.fqdn]
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,3 +11,13 @@ locals {
     TerraformConfigurationURL = "https://github.com/wellcomecollection/wellcomeimages/tree/master/terraform"
   }
 }
+
+provider "aws" {
+  region  = "eu-west-1"
+  version = "~> 2.69"
+  alias   = "dns"
+
+  assume_role {
+    role_arn = "arn:aws:iam::267269328833:role/wellcomecollection-assume_role_hosted_zone_update"
+  }
+}
